1bb12c05d6e9fe46b3bcb76c41d7838400835e74
bob-donnelly <bob-donnelly@live.nmit.ac.nz>
2020-10-30 15:52:02 +1300

    javascript variables

---
+++ src/index.js
@@ -0,0 +1,29 @@
+// Variable named as var firstname = Bob ln = Donnelly
+
+var fn = 'Bob'
+var ln = 'Donnelly'
+
+// Using let to do the same as var 
+// Use let if you need to reassign the value while keeping the original
+
+let h = 'rental'
+let c = '90s'
+
+// Using const to do the same as above
+// Const is a constant variable like var where it is fixed
+
+const p = 'dog'
+const p2 = 'cat'
+
+var fVar
+var sVar
+var tVar
+
+let fVar
+let sVar
+let tVar
+
+var fVar, sVar, tVar
+let fVar, sVar, tVar
+
+console.log(fn, ln, p, h, c, p2)
